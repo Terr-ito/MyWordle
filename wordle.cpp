99fa6e8dcff43bd339397c732f17ef088b31819c
Razvan <oprutarazvanterr@gmail.com>
2026-07-13 14:38:13 +0300

    Added duplicate letter edge cases.

--- wordle.cpp
+++ wordle.cpp
@@ -4,9 +4,6 @@
 #include <string>
 #include <chrono>
 
-// Missing duplicate letter edge cases
-
-
 // Struct for a wordle type word
 // type = 0 = grey ----- letter has not been checked
 // type = 1 = black ---- letter does not appear
@@ -17,11 +14,22 @@ typedef struct {
     unsigned type[5];
 }wordle;
 
+// Counts the number of occurences of a char in a string
+int countCharacter(std::string s, char c) {
+    int count = 0;
+ 
+    for(unsigned i = 0; i < s.length(); i++) {
+        if(s[i] == c) count++;
+    }
+
+    return count;
+}
+
 void showKeyboard(unsigned letters[]) {
     for(int i = 0; i < 26; i++) {
         std::cout << char('a' + i) << " - ";
 
-        // Translate unsigned type to color
+        // Unsigned type to color
         if(letters[i] == 0) std::cout << "grey";
         else if(letters[i] == 1) std::cout << "black";
         else if(letters[i] == 2) std::cout << "yellow";
@@ -56,6 +64,39 @@ void isValid(std::string &validWord, std::vector<std::string> database) {
     }
 }
 
+// 1st "for" is a surface level check
+// 2nd "for" is for duplicate letter edge cases
+void checkGuess(std::string word, wordle &guess) {
+    // 1st
+    for(int l = 0; l < 5; l++) {
+        // Letter is green
+        if(guess.word[l] == word[l]) guess.type[l] = 3;
+        // Letter is yellow (for now)
+        else if(word.find(guess.word[l]) != std::string::npos) guess.type[l] = 2;
+        // Letter is black
+        else guess.type[l] = 1;
+    }
+
+    // 2nd - yellow letter edge cases
+    for(int l = 0; l < 5; l++) {
+        if(guess.type[l] == 2) {
+            // Checks if there are more duplicate letter guesses than in the word
+            if(countCharacter(guess.word, guess.word[l]) > countCharacter(word, guess.word[l])) {
+                guess.word[l] = toupper(guess.word[l]);
+                // Letter is black
+                guess.type[l] = 1;
+            }
+        }
+    }
+
+    // Display guess results
+    for(int l = 0; l < 5; l++) {
+        guess.word[l] = tolower(guess.word[l]);
+        std::cout << guess.word[l] << " - " << guess.type[l] << "\n";
+    }
+
+}
+
 int main(int argc, char** argv) {
     // Open wordle words text file
     std::ifstream wordDB("valid-wordle-words.txt");
@@ -88,8 +129,9 @@ int main(int argc, char** argv) {
     }
 
     // Pulled word
-    std::cout << word << "\n";
-
+    // std::cout << word << "\n";
+    std::cout << "You can begin guessing." << "\n" << "5 letter words only!" << "\n";
+    
     // Start guessing loop (i = guess count) - 6 guesses
     unsigned i = 0;
     while(i < 6) {
@@ -97,21 +139,7 @@ int main(int argc, char** argv) {
         isValid(guess[i].word, database);
 
         // Check each letter of the word (l = letter)
-        for(int l = 0; l < 5; l++) {
-            if(guess[i].word[l] == word[l]) {
-                // letter is green
-                guess[i].type[l] = 3;
-            }
-            else {
-                if(word.find(guess[i].word[l]) != std::string::npos) {
-                    //letter is yellow
-                    guess[i].type[l] = 2;
-                }
-                //letter is black
-                else guess[i].type[l] = 1;
-            }
-            std::cout << guess[i].word[l] << " - " << guess[i].type[l] << "\n";
-        }
+        checkGuess(word, guess[i]);
 
         // Update letter type to wordle keyboard
         letters[guess[i].word[0] - 'a'] = guess[i].type[0];
@@ -123,7 +151,7 @@ int main(int argc, char** argv) {
 
         // All characters green = we found the word
         if(guess[i].type[0] == 3 && guess[i].type[1] == 3 && guess[i].type[2] == 3 && guess[i].type[3] == 3 && guess[i].type[4] == 3) {
-            std::cout << "Congrats.";
+            std::cout << "Congrats!";
             return 0;
         }
 
@@ -132,6 +160,7 @@ int main(int argc, char** argv) {
     }
     
     // Failed to guess word
+    std::cout << "The word was \"" << word << "\"." << "\n";
     std::cout << "Better luck next time.";
     return 0;
 }
